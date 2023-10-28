de307488ed3b98dde81d992c87ce6b27c7005919
Angelo Arcillas <angeloarcillas64@gmail.com>
2023-10-28 19:31:51 +0800

    init vue ts tailwindcss

---
+++ src/components/Forms/InputField.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+interface InputFieldProps {
+  type?: string;
+}
+
+withDefaults(defineProps<InputFieldProps>(), {
+  type: 'text',
+});
+</script>
+
+<template>
+  <input type="type" class="input" />
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import InputField from './Forms/InputField.vue';
+
+const inputField = ref('');
+</script>
+
+<template>
+  <!-- Heading -->
+  <section class="section">
+    <h1 class="heading-1">Heading 1</h1>
+    <h2 class="heading-2">Heading 2</h2>
+    <h3 class="heading-3">Heading 3</h3>
+  </section>
+
+  <!-- Forms -->
+  <section class="section">
+    <InputField :value="inputField"  />
+  </section>
+</template>
